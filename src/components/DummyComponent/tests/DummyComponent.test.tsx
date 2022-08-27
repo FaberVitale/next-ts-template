b1bfc9e39e54fd797a45db57ef8bb70c1b97eed8
FaberVitale <faber.vitale@gmail.com>
2022-08-27 10:11:40 +0200

    chore(testing): add note about testing api controllers and react components

--- src/components/DummyComponent/tests/DummyComponent.test.tsx
+++ src/components/DummyComponent/tests/DummyComponent.test.tsx
@@ -1,6 +1,9 @@
 import { DummyComponent } from '../DummyComponent';
 import { render, screen } from '@testing-library/react';
 
+// @see https://testing-library.com/docs/react-testing-library/intro/
+// @see https://jestjs.io/docs/getting-started
+// @see https://testing-library.com/docs/queries/about
 describe('DummyComponent', () => {
   it('renders a div', () => {
     render(
--- src/pages/api/tests/hello.test.ts
+++ src/pages/api/tests/hello.test.ts
@@ -1,5 +1,9 @@
 import helloHandler from '../hello';
 
+// endpoint handlers are simple functions,
+// use https://www.npmjs.com/package/node-mocks-http or https://www.npmjs.com/package/next-test-api-route-handler
+// If you need to test them.
+// @see https://nextjs.org/docs/api-routes/introduction
 describe('api/hello', () => {
   it('exports a function as default import', () => {
     expect(typeof helloHandler).toBe('function');
