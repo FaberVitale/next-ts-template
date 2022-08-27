cec7a6de477a0f5b87892c56d2f5d9193608fd1c
FaberVitale <faber.vitale@gmail.com>
2022-08-27 17:31:48 +0200

    chore(tests): add fetch mock

--- src/components/DummyComponent/tests/DummyComponent.test.tsx
+++ src/components/DummyComponent/tests/DummyComponent.test.tsx
@@ -1,5 +1,6 @@
 import { DummyComponent } from '../DummyComponent';
 import { render, screen } from '@testing-library/react';
+import fetchMock from 'jest-fetch-mock';
 
 // @see https://testing-library.com/docs/react-testing-library/intro/
 // @see https://jestjs.io/docs/getting-started
@@ -11,9 +12,16 @@ describe('DummyComponent', () => {
         <div data-testid="child" />
       </DummyComponent>
     );
-
     const testChild = screen.getByTestId('child');
 
     expect(testChild.parentElement?.tagName?.toLowerCase()).toBe('div');
+  });
+
+  test('fetches correctly', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify('punk'));
+
+    const res = await (await fetch('/pink')).json();
+
+    expect(res).toBe('punk');
   });
 });
