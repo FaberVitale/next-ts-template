0df24a358c477fb3c733febb53db157fc9e67ce8
FaberVitale <faber.vitale@gmail.com>
2022-08-31 15:05:45 +0200

    chore(e2e): add more e2e test examples

--- cypress/e2e/index.cy.ts
+++ cypress/e2e/index.cy.ts
@@ -5,13 +5,26 @@ import type {} from 'cypress';
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-describe('Navigation', () => {
+describe('Index page', () => {
   it('index page should have correct title', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/');
 
     // Has correct title
     cy.get('title').contains('Welcome to Next.js!');
+  });
+
+  it('index page has correct footer links', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/');
+
+    // Footer has link to template source
+    cy.get(
+      '#__next footer a[href^="https://github.com/FaberVitale/next-ts-template"]'
+    );
+
+    // Footer has link to nextjs docs
+    cy.get('#__next footer a[href^="https://nextjs.org/docs"]');
   });
 });
 
